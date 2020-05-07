3931cbe7f9072e2dd46ed9868ad051c8094a2e6c
motababravo <motaba@gmail.com>
2020-05-07 10:08:47 +0300

    Update wvc-tool.py

--- wvc-tool.py
+++ wvc-tool.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 
 import serial
+# Linux
 ser = serial.Serial("/dev/ttyUSB0", 9600)
-###ser = serial.Serial("COM5", 9600)
+# Windows
+# ser = serial.Serial("COM5", 9600)
+# Ser2Net
+# ser = serial.serial_for_url('rfc2217://192.168.78.13:7000', timeout=1)
 
+# AutoGet Modem ID
 getmid = b'\xFA\xFF\xFF\xFF\xFF\xFF\xFF\x6D' # get modem ID
 ser.write(getmid)
 s = ser.read(8)
 #print(s)
-##print(bytes(s).hex())
+#print(bytes(s).hex())
 id = s [1:-3]
 #print("Reading modem ID ... :" , bytes(id).hex())
 print(bytes(id).hex())
